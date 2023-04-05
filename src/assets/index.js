8c19afa46d32bdc1be9e5361ad6da348d422a042
carlosalbertojusto <carlosjustodev@gmail.com>
2023-04-04 20:06:00 -0400

    add: import Images

---
+++ src/assets/index.js
@@ -0,0 +1,39 @@
+import ImageBatman from "./batman.png"
+import ImageChloe from "./chloe.png"
+import ImageDontBreathe from "./dontbreathe.png"
+import ImageDune from "./dune.png"
+import ImageDunkirk from "./dunkirk.png"
+import ImageImdb from "./imdb.png"
+import ImageJohnWick from "./johnwick.png"
+import ImageKeanu from "./Keanu.png"
+import ImageLamb from "./lamb.png"
+import ImageLogotv from "./logotv.png"
+import ImageNoTimeToDie from "./noTimeToDie.png"
+import ImageRottenTomatoes from "./rottentomatoe.png"
+import ImageStrangerThings from "./strangerthings.png"
+import ImageRyan from "./Ryan.png"
+import ImageWillThereBe from "./willtherebe.png"
+import ImageShangchi from "./shangchi.png"
+import ImageSpiderman from "./Spiderman.png"
+import ImageSuicideSquad from "./suicidesquad.png"
+
+export const images = {
+  ImageBatman,
+  ImageChloe,
+  ImageDontBreathe,
+  ImageDune,
+  ImageDunkirk,
+  ImageImdb,
+  ImageJohnWick,
+  ImageKeanu,
+  ImageLamb,
+  ImageLogotv,
+  ImageNoTimeToDie,
+  ImageRottenTomatoes,
+  ImageStrangerThings,
+  ImageRyan,
+  ImageWillThereBe,
+  ImageShangchi,
+  ImageSpiderman,
+  ImageSuicideSquad,
+}
